44f05e27e432d845c8023675fa4a52c5e3ff2556
Muhammed Anwar <muhammedanwarwork@gmail.com>
2026-04-24 19:57:53 +0300

    Auto Deployment Test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Project Successfully Deployed! </h1><p>Running inside Docker via GitHub Actions.</p>"
+    return "<h1>DevOps Phase 4 Completed! Auto-Deployed via GitHub Actions.</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
